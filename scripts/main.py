53a7050d8f6d4a92281403d04fbf2f2c59baf5a8
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2022-01-28 19:44:21 +0900

    Implement script for dataset generation

--- scripts/main.py
+++ scripts/main.py
@@ -1,123 +1,114 @@
+import os
 import sys
+os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 sys.path.append(".")
 sys.path.append("..")
 
-import open3d as o3d
-import numpy as np
-import matplotlib.pyplot as plt
-import pyrender
+import argparse
+import time
+import multiprocessing as mp
+from joblib import Parallel, delayed
+from itertools import repeat
 
+import cv2
+import numpy as np
+
+from render import *
 from utils.math import *
 
-def render_mesh_o3d(
-        mesh_path: str,
-        save_path: str,
-        theta: float,
-        phi: float,
-        height: int,
-        width: int,
-        znear: float = 0.01,
-        zfar: float = 1500,
+def _render_shapenet_sample(
+    shapenet_src_dir: str, sample_id: str, 
+    save_dir: str,
+    height: int, width: int,
     ) -> None:
-    """
-    Renders a mesh loaded as open3d.geometry.TriangleMesh object.
+    sample_dir = os.path.join(shapenet_src_dir, str(sample_id))
+    assert os.path.exists(sample_dir), "[!] Path {} does not exist".format(sample_dir)
 
-    Args:
-    - mesh_path: String.
-        Path to where the mesh is stored.
-    - save_path: String.
-        Path to where the rendered image will be saved.
-    - theta: Float.
-        Angle between positive direction of y axis and displacement vector.
-    - phi: Float.
-        Angle between positive direction of x axis and displacement vector.
-    - height: Int.
-        Height of the viewport.
-    - width: Int.
-        Width of the viewport.
-    - znear: Float.
-        The nearest visible depth.
-    - zfar: Float.
-        The farthest visible depth.
-    """
-    # set camera intrinsics
-    fx = 39.227512 / 0.0369161
-    fy = 39.227512 / 0.0369161
-    K = build_camera_intrinsic(fx, fy, height, width)
-    
-    # set camera extrinsics
-    E = build_camera_extrinsic(
-        1.0, theta, phi,
-        np.array([0., 1., 0.])
-    )
+    # if not a directory, ignore and quit
+    if not os.path.isdir(sample_dir):
+        return
 
-    # load mesh and process
-    mesh = o3d.io.read_triangle_mesh(str(mesh_path))
+    # create directories for outputs
+    sample_save_dir = os.path.join(save_dir, str(sample_id))
+    sample_img_dir = os.path.join(sample_save_dir, "image")
+    sample_depth_dir = os.path.join(sample_save_dir, "depth")
+    sample_mask_dir = os.path.join(sample_save_dir, "mask")
+
+    if not os.path.exists(sample_save_dir):
+        os.mkdir(sample_save_dir)
+    if not os.path.exists(sample_img_dir):
+        os.mkdir(sample_img_dir)
+    if not os.path.exists(sample_depth_dir):
+        os.mkdir(sample_depth_dir)
+    if not os.path.exists(sample_mask_dir):
+        os.mkdir(sample_mask_dir)
+
+    # specify the file name of the mesh
+    mesh_file = os.path.join(sample_dir, "models/model_normalized.obj")
+    assert os.path.exists(mesh_file), "[!] Mesh file does not exist at {}".format(mesh_file)
+
+    # specify camera intrinsics and extrinsics
+    phis = np.linspace(0, 2 * np.pi, 24)  # divide 360 degrees into 24 steps
+    thetas = (np.pi / 2.25) * np.ones_like(phis)  # fixed elevation
+
+    # load mesh
+    mesh = o3d.io.read_triangle_mesh(mesh_file)
     mesh.compute_vertex_normals()
     mesh.paint_uniform_color((0.7, 0.7, 0.7))
     box = mesh.get_axis_aligned_bounding_box()
     mesh_scale = ((box.get_max_bound() - box.get_min_bound()) ** 2).sum()
+    mesh = mesh.scale(0.2 * mesh_scale, center=(0, 0, 0))
 
-    mesh = mesh.scale(0.3 * mesh_scale, center=(0, 0, 0))
-    verts = np.asarray(mesh.vertices).astype(np.float32)
-    faces = np.asarray(mesh.triangles).astype(np.int32)
-    colors = np.asarray(mesh.vertex_colors).astype(np.float32)
-    normals = np.asarray(mesh.vertex_normals).astype(np.float32)
+    # render and save the results
+    for view_idx, (theta, phi) in enumerate(zip(thetas, phis)):
+        img, depth, mask = render_mesh_o3d(
+            mesh,
+            theta,
+            phi,
+            height, width,
+        )
+    
+        cv2.imwrite(os.path.join(sample_img_dir, "{}.jpg".format(view_idx)), img)
+        cv2.imwrite(os.path.join(sample_depth_dir, "{}.exr".format(view_idx)), depth)
+        cv2.imwrite(os.path.join(sample_mask_dir, "{}.jpg".format(view_idx)), mask)
 
-    scene = pyrender.Scene()
+def render_shapenet_samples(
+    shapenet_src_dir: str,
+    save_dir: str,
+    height: int, width: int,
+    ) -> None:
 
-    # add mesh
-    mesh = pyrender.Mesh(
-        primitives=[
-            pyrender.Primitive(
-                positions=verts,
-                normals=normals,
-                color_0=colors,
-                indices=faces,
-                mode=pyrender.GLTF.TRIANGLES,
-            )
-        ],
-        is_visible=True,
+    # get sample directories
+    sample_ids = [d for d in os.listdir(shapenet_src_dir)]
+
+    # create the save directory
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    
+    start_t = time.time()
+    
+    num_cores = mp.cpu_count() // 2
+    print("[!] Using {} cores".format(num_cores))
+    _ = Parallel(n_jobs=num_cores)(
+        delayed(_render_shapenet_sample)(src_dir, sample_id, out_dir, H, W) \
+            for src_dir, sample_id, out_dir, H, W in \
+            zip(repeat(shapenet_src_dir), sample_ids, repeat(save_dir), repeat(height), repeat(width))
     )
-    mesh_node = pyrender.Node(mesh=mesh, matrix=np.eye(4))
-    scene.add_node(mesh_node)
 
-    # add camera
-    cam = pyrender.IntrinsicsCamera(
-        fx=K[0, 0],
-        fy=K[1, 1],
-        cx=K[0, 2],
-        cy=K[1, 2],
-        znear=znear,
-        zfar=zfar,
-    )
-
-    cam_node = pyrender.Node(camera=cam, matrix=E)
-    scene.add_node(cam_node)
-
-    # add light
-    light = pyrender.DirectionalLight(color=np.ones(3), intensity=3)
-    light_node = pyrender.Node(light=light, matrix=np.eye(4))
-    scene.add_node(light_node, parent_node=cam_node)
-
-    render = pyrender.OffscreenRenderer(width, height)
-    color, depth = render.render(scene)
-
-    plt.imshow(depth)
-    plt.show()
-
-    plt.imshow(color)
-    plt.show()
-
+    print("[!] Took {} seconds".format(time.time() - start_t))
+    print("[!] Done.")
 
 if __name__ == "__main__":
-    path = "./data/models2/models/model_normalized.obj"
-    out_file = "./depth.png"
-    render_mesh_o3d(
-        path, 
-        out_file, 
-        theta=np.pi/3, 
-        phi=-np.pi/2, 
-        width=500, height=500
-    )
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--shapenet_path", type=str, default="./data")
+    parser.add_argument("--save_path", type=str, default="./result")
+    parser.add_argument("--height", type=int, default=192)
+    parser.add_argument("--width", type=int, default=256)
+    args = parser.parse_args()
 
+    # render
+    render_shapenet_samples(
+        args.shapenet_path, "result", 
+        args.height, args.width
+        )
